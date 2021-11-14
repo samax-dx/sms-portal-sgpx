fa47342ec570c2640c2f99e3314a8ee20881c6c1
samax-dx <samax.mw@yahoo.com>
2021-11-14 13:06:14 +0600

    fixed QForEach index issue

--- QForEach.cs
+++ QForEach.cs
@@ -9,7 +9,7 @@ namespace sms_portal_sgp
         public static void ForEach<T>(this IEnumerable<T> e, Action<T, int> cb)
         {
             var index = 0;
-            foreach (var item in e) cb(item, index);
+            foreach (var item in e) cb(item, index++);
         }
     }
 }
